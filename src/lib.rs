74abb28a8d970bd1c54cbd76f25edba8e0e24602
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-08 17:58:31 +0000

    Write an is_prime function

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,15 @@ extern "C" {
 pub fn greet() {
     alert("Hello, wasm-prime!");
 }
+
+fn is_prime(n: u64) -> bool {
+    if n == 0 || n == 1 {
+        return false;
+    }
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
