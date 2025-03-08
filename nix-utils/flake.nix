34669ccf9feed76382dd715ac919bcfb36a0eabd
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-08 17:31:45 +0000

    Put wasm-pack in direnv

---
+++ nix-utils/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/de0fe301211c";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              wasm-pack
+            ];
+          };
+        }
+    );
+}
